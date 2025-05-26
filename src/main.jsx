274f272088629e25d90757420a0635bd4a966c8e
Mathieu <Matt@soundfingers.com>
2025-05-26 13:48:24 +0200

    added robots cards

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import 'tachyons';
+import Card from './Card.jsx';
+import CardList from './CardList.jsx';
+import { robots } from './robots.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CardList robots={robots}/>
   </StrictMode>,
 )
